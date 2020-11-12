ffdae81232c994dac762ba67eeac0e08c051795d
Marie Lepers <mv.lepers@gmail.com>
2020-11-12 17:07:16 +0100

    improv app

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,13 +4,14 @@ require 'json'
 class GamesController < ApplicationController
 
     def new
-        @grid = []
-        @letter = ""
+        @letters = []
+        # @letter = ""
         10.times do
-            @grid << ('A'..'Z').to_a.sample
+            @letters << ('A'..'Z').to_a.sample
         end
+
     end
-    
+
 
     def score
         @score = ""
@@ -18,20 +19,24 @@ class GamesController < ApplicationController
         word_url = 'https://wagon-dictionary.herokuapp.com/' + @word
 
         word_serialized = open(word_url).read
-        @word2 = JSON.parse(word_serialized)
+        @api_result = JSON.parse(word_serialized)
 
-        @word_in_array = @word.chars
+
+        # @word_in_array = @word.chars
         @my_word = @word.upcase.chars.sort
         @the_grid = params[:grid].chars.sort
 
        @condition_grid = compare_grid_and_word(@the_grid, @my_word)
 
-        if @condition_grid && @word2['found']
-                @score = "well done"
-        elsif !@condition_grid && !@word2['found']
+        if @condition_grid && @api_result['found']
+            @score = "is a good word (english and in the grid)."
+            @message = "Good Job,"
+        elsif !@condition_grid && !@api_result['found']
             @score = "is not an english word and not in the grid"
+            @message = "Sorry"
         else
             @score ="is not in the grid"
+             @message = "Sorry"
         end
 
     end
@@ -40,10 +45,10 @@ class GamesController < ApplicationController
     def compare_grid_and_word(grid, attempt)
         grid_hash = Hash.new(0)
         grid.each { |letter| grid_hash[letter.downcase] += 1 }
-      
+
         attempt_hash = Hash.new(0)
         attempt.each { |letter| attempt_hash[letter.downcase] += 1 }
-      
+
         attempt_hash.each do |key, value|
           return false unless grid_hash.key?(key) && value <= grid_hash[key]
         end
